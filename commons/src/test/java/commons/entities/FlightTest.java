3e733045c74f0bd81c486949ae05010f4b3b737e
Ignacio Cuñado Barral <116840139+ignaciocunado@users.noreply.github.com>
2025-12-30 17:19:08 +0100

    fix: Assemble gradle instead of build + sample tests

---
+++ commons/src/test/java/commons/entities/FlightTest.java
@@ -0,0 +1,13 @@
+package commons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
--- server/src/test/java/server/FlightsApiApplicationTests.java
+++ server/src/test/java/server/FlightsApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.booking.flights_api;
+package server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
