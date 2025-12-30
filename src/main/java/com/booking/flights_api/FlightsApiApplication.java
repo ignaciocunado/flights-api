5c7647248ca77e16b2221b3c6b43adbd4ec171e3
Ignacio Cuñado Barral <ignacunado@gmail.com>
2025-12-30 12:58:53 +0100

    feat: Initial commit

---
+++ src/main/java/com/booking/flights_api/FlightsApiApplication.java
@@ -0,0 +1,13 @@
+package com.booking.flights_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsApiApplication.class, args);
+	}
+
+}
