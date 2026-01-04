b13d10bbdbb23e94bba0ea06ed13365fd023f56b
Ignacio Cuñado Barral <ignacunado@gmail.com>
2026-01-04 14:02:51 +0100

    feat: Add flight object and tests

--- commons/src/main/java/commons/entities/Flight.java
+++ commons/src/main/java/commons/entities/Flight.java
@@ -2,9 +2,11 @@ package commons.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
@@ -13,10 +15,96 @@ public final class Flight {
 
     @Id
     @GeneratedValue
+    @Getter
     private Long id;
 
+    @Getter
+    @Setter
+    @NotBlank
+    private String flightNumber;
+
+    @Getter
+    @Setter
+    @NotNull
+    private String airline;
+
+    @Getter
+    @Setter
+    @NotNull
+    private String origin;
+
+    @Getter
+    @Setter
+    @NotNull
+    private String destination;
+
+    @Getter
+    @Setter
+    private LocalDateTime departureTime;
+
+    @Getter
+    @Setter
+    private LocalDateTime arrivalTime;
+
+    @Getter
+    @Setter
+    private String departureTimezone;
+
+    @Getter
+    @Setter
+    private String arrivalTimezone;
+
+    @Getter
+    @Setter
+    private int duration; // in minutes
+
+    @Getter
+    @Setter
+    private int price; // in cents to avoid floating point arithmetic
+
+    @Getter
+    @Setter
+    private String currency;
+
+    @Getter
+    @Setter
+    private int availableSeats;
+
+    public Flight(String flightNumber, String airline, String origin, String destination, LocalDateTime departureTime, String departureTimezone, LocalDateTime arrivalTime, String arrivalTimezone, int duration, int price, String currency, int availableSeats) {
+        this.flightNumber = flightNumber;
+        this.airline = airline;
+        this.origin = origin;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.departureTimezone = departureTimezone;
+        this.arrivalTime = arrivalTime;
+        this.arrivalTimezone = arrivalTimezone;
+        this.duration = duration;
+        this.price = price;
+        this.currency = currency;
+        this.availableSeats = availableSeats;
+    }
+
+    public boolean canBeBooked() {
+        return this.availableSeats > 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Flight flight)) return false;
+        return Objects.equals(id, flight.id)
+                && Objects.equals(flightNumber, flight.flightNumber)
+                && Objects.equals(airline, flight.airline)
+                && Objects.equals(origin, flight.origin)
+                && Objects.equals(destination, flight.destination)
+                && Objects.equals(departureTime, flight.departureTime)
+                && Objects.equals(arrivalTime, flight.arrivalTime)
+                && Objects.equals(departureTimezone, flight.departureTimezone)
+                && Objects.equals(arrivalTimezone, flight.arrivalTimezone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, flightNumber, airline, origin, destination, departureTime, arrivalTime, departureTimezone, arrivalTimezone);
+    }
 }
-
-
-//flight_number, airline, origin, destination,
-//departure time, arrival time, duration, and price
--- commons/src/test/java/commons/entities/FlightTest.java
+++ commons/src/test/java/commons/entities/FlightTest.java
@@ -1,13 +1,166 @@
-package commons;
+package commons.entities;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlightTest {
 
+    private Flight flight1;
+    private Flight flight2;
+    private Flight flight3;
+
+    @BeforeEach
+    void setUp() {
+        LocalDateTime departure = LocalDateTime.parse("2025-01-01T00:00:00");
+        LocalDateTime arrival = LocalDateTime.parse("2025-01-01T02:00:00");;
+        this.flight1 = new Flight("AB1234", "Ignacio AIR", "AGP", "AMS", departure, "CET", arrival, "CET", 120, 10000, "EUR", 100);
+        this.flight2 = new Flight("AB1234", "Ignacio AIR", "AGP", "AMS", departure, "CET", arrival, "CET", 120, 10000, "EUR", 100);
+        this.flight3 = new Flight("AB1233", "Booking AIR", "AGP", "AMS", departure, "CET", arrival, "CET", 120 ,100, "EUR", 29);
+    }
+
     @Test
-    public void sampleTest() {
-        assertTrue(true);
+    void testEqualsGoodWeather() {
+        assertEquals(this.flight1, this.flight2);
+    }
+
+    @Test
+    void testEqualsBadWeather() {
+        assertNotEquals(this.flight1, this.flight3);
+    }
+
+    @Test
+    void getFlightNumber() {
+        assertEquals("AB1234", this.flight1.getFlightNumber());
+    }
+
+    @Test
+    void getAirline() {
+        assertEquals("Ignacio AIR", this.flight1.getAirline());
+    }
+
+    @Test
+    void getOrigin() {
+        assertEquals("AGP", this.flight1.getOrigin());
+    }
+
+    @Test
+    void getDestination() {
+        assertEquals("AMS", this.flight1.getDestination());
+    }
+
+    @Test
+    void getDepartureTime() {
+        assertEquals(LocalDateTime.parse("2025-01-01T00:00:00"), this.flight1.getDepartureTime());
+    }
+
+    @Test
+    void getArrivalTime() {
+        assertEquals(LocalDateTime.parse("2025-01-01T02:00:00"), this.flight1.getArrivalTime());
+    }
+
+    @Test
+    void getDepartureTimezone() {
+        assertEquals("CET", this.flight1.getDepartureTimezone());
+    }
+
+    @Test
+    void getArrivalTimezone() {
+        assertEquals("CET", this.flight1.getArrivalTimezone());
+    }
+
+    @Test
+    void getDuration() {
+        assertEquals(120, this.flight1.getDuration());
+    }
+
+    @Test
+    void getPrice() {
+        assertEquals(10000, this.flight1.getPrice());
+    }
+
+    @Test
+    void setFlightNumber() {
+        this.flight1.setFlightNumber("some other flight number");
+        assertEquals("some other flight number", this.flight1.getFlightNumber());
+    }
+
+    @Test
+    void setAirline() {
+        this.flight1.setAirline("some other airline");
+        assertEquals("some other airline", this.flight1.getAirline());
+    }
+
+    @Test
+    void setOrigin() {
+        this.flight1.setOrigin("other origin");
+        assertEquals("other origin", this.flight1.getOrigin());
+    }
+
+    @Test
+    void setDestination() {
+        this.flight1.setDestination("other destination");
+        assertEquals("other destination", this.flight1.getDestination());
+    }
+
+    @Test
+    void setDepartureTime() {
+        this.flight1.setDepartureTime(LocalDateTime.parse("2025-01-01T00:00:00").plusDays(2));
+        assertEquals(LocalDateTime.parse("2025-01-01T00:00:00").plusDays(2), this.flight1.getDepartureTime());
+    }
+
+    @Test
+    void setArrivalTime() {
+        this.flight1.setArrivalTime(LocalDateTime.parse("2025-01-01T00:00:00").plusDays(2));
+        assertEquals(LocalDateTime.parse("2025-01-01T00:00:00").plusDays(2), this.flight1.getArrivalTime());
+    }
+
+    @Test
+    void setDepartureTimezone() {
+        this.flight1.setDepartureTimezone("UTC");
+        assertEquals("UTC", this.flight1.getDepartureTimezone());
+    }
+
+    @Test
+    void setArrivalTimezone() {
+        this.flight1.setArrivalTimezone("UTC");
+        assertEquals("UTC", this.flight1.getArrivalTimezone());
+    }
+
+    @Test
+    void setDuration() {
+        this.flight1.setDuration(-1);
+        assertEquals(-1, this.flight1.getDuration());
+    }
+
+    @Test
+    void setPrice() {
+        this.flight1.setPrice(-1);
+        assertEquals(-1, this.flight1.getPrice());
+    }
+
+    @Test
+    void getAvailableSeats() {
+        assertEquals(100, this.flight1.getAvailableSeats());
+    }
+
+    @Test
+    void setAvailableSeats() {
+        this.flight1.setAvailableSeats(-1);
+        assertEquals(-1, this.flight1.getAvailableSeats());
+    }
+
+    @Test
+    void canBeBooked() {
+        assertTrue(this.flight1.canBeBooked());
+    }
+
+    @Test
+    void canNotBeBooked() {
+        this.flight1.setAvailableSeats(-1);
+        assertFalse(this.flight1.canBeBooked());
     }
 }
